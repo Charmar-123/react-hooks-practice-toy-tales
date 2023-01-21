9c268ca1b1e7a128da0fffb2e2623f70fb4255bb
Charbel Maroun <charbel-maroun@hotmail.com>
2023-01-21 13:06:49 -0500

    commit tree 7be0ec5bd53a0764605efa5c543c129b4b1d86d7

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,40 @@
-import React from "react";
 
-function ToyCard() {
+
+function ToyCard({name, image, likes, id, handleDelete, handleLike}) {
+
+
+
+  const onDeleteClick = () =>{
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method : "DELETE",
+    })
+    .then(res => res.json())
+    .then(() => handleDelete(id))
+  }
+
+  const onUpdateClick = () => {
+
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method : "PATCH",
+      headers : {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({likes : likes + 1})
+    })
+    .then(res => res.json())
+    .then(data => handleLike(data.likes))
+  }
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={onUpdateClick}>Like {"<3"}</button>
+      <button className="del-btn" onClick={onDeleteClick}>Donate to GoodWill</button>
     </div>
   );
 }
